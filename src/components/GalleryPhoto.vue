f2fb4706bbbd1028f93571082b8f2f85626d0ed8
Ckiv <baltazar1995>
2022-03-29 11:09:14 +0300

    feat: add gallery

--- src/components/GalleryPhoto.vue
+++ src/components/GalleryPhoto.vue
@@ -1,5 +1,11 @@
 <template>
-<h1>Gallery</h1>
+<div class="gallery">
+  <img src="../../public/picture/gallery-1.jpg" alt="gallery image" class="gallery__img-1">
+  <img src="../../public/picture/gallery-2.jpg" alt="gallery image" class="gallery__img-2">
+  <img src="../../public/picture/gallery-3.jpg" alt="gallery image" class="gallery__img-3">
+  <img src="../../public/picture/gallery-4.jpg" alt="gallery image" class="gallery__img-4">
+  <img src="../../public/picture/gallery-5.jpg" alt="gallery image" class="gallery__img-5">
+</div>
 </template>
 
 <script>
@@ -9,5 +15,26 @@ export default {
 </script>
 
 <style scoped>
-
+.gallery {
+  display: grid;
+  grid-template-areas:
+        "gallery__img-1 gallery__img-2 gallery__img-3"
+        "gallery__img-1 gallery__img-4 gallery__img-5";
+  grid-gap: 16px;
+}
+.gallery__img-1 {
+  grid-area: gallery__img-1;
+}
+.gallery__img-2 {
+  grid-area: gallery__img-2;
+}
+.gallery__img-3 {
+  grid-area: gallery__img-3;
+}
+.gallery__img-4 {
+  grid-area: gallery__img-4;
+}
+.gallery__img-5 {
+  grid-area: gallery__img-5;
+}
 </style>
